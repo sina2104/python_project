2c68204d70109219a0fff005bf8b5fa37b1b2a77
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-04-19 20:26:40 +0300

    Update tamagotchi.py

--- tamagotchi.py
+++ tamagotchi.py
@@ -1,8 +1,12 @@
 from random import randrange
+
 from animal import Animal
 
 
 class Play(Animal):
+    """
+        our_animal - наше животное
+    """
     from time import sleep
 
     def __init__(self):
@@ -14,29 +18,29 @@ class Play(Animal):
         try:
             pet = int(input())
             types = {1: Animal.Cat, 2: Animal.Dog}
-            play1 = types[pet]
-            play1()
+            our_animal = types[pet]
+            our_animal()
             break
         except KeyError:
             print("choose again:")
         except ValueError:
             print("choose again:")
-    while 1:
-        play1.pet.clock_tick()
-        print(play1.pet, '\n' + play1.pet.sounds[0])
+    while True:
+        our_animal.pet.clock_tick()
+        print(our_animal.pet, '\n' + our_animal.pet.sounds[0])
         sleep(3)
-        if play1.pet.mood() == 'dead':
+        if our_animal.pet.mood() == 'dead':
             print('You had to care about me...')
-            print(play1.pet.dead_look)
+            print(our_animal.pet.dead_look)
             break
-        if play1.pet.mood() == 'hungry':
+        if our_animal.pet.mood() == 'hungry':
             food = input()
-            if food in play1.pet.diet:
-                play1.pet.feed()
-        elif play1.pet.mood() == 'bored':
+            if food in our_animal.pet.diet:
+                our_animal.pet.feed()
+        elif our_animal.pet.mood() == 'bored':
             entertain = input()
-            play1.pet.teach(entertain)
-            print(play1.pet.sounds[randrange(1, len(play1.pet.sounds))])
+            our_animal.pet.teach(entertain)
+            print(our_animal.pet.sounds[randrange(1, len(our_animal.pet.sounds))])
 
 
 tamagotchi = Play()
