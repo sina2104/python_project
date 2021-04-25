eb9e6a1db26052d3baf4271c83d2d4140c3b8d4b
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-04-25 22:14:33 +0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,27 +1,46 @@
 from pet import Pet
+"""
+    тут мы определяем вид животного
+"""
 
 
-class Animal(Pet):
-    """
-       тут мы определяем вид животного
-    """
-    def __init__(self):
+class Cat(Pet):
+    def __init__(self):      
         super().__init__()
+    pet = Pet()
+    pet.diet = ['chicken', 'turkey', 'apple']
+    pet.name = 'kitty'
+    pet.look = '''
+     /\_/\ 
+    ( o.o )
+     > ^ <'''
+    pet.dead_look = '''
+     /\_/\ 
+    ( *.* )
+     > ^ < '''
+    pet.sounds.append('Mrrrr')
+    print(f"I'm a {pet.name}{pet.look}")
 
-    class Cat:
-        pet = Pet()
-        pet.diet = ['chicken', 'turkey', 'apple']
-        pet.name = 'kitty'
-        pet.look = ' /\_/\ \n( o.o )\n > ^ <'
-        pet.dead_look = ' /\_/\ \n( *.* )\n > ^ <'
-        pet.sounds.append('Mrrrr')
-        print("I'm a " + pet.name, '\n' + pet.look)
 
-    class Dog:
-        pet = Pet()
-        pet.diet = ['beef', 'bone', 'carrot']
-        pet.name = 'big dog'
-        pet.look = '  /^ ^\ \n / 0 0 \ \n V\ Y /V \n  / - \ \n /    | \nV__) ||'
-        pet.dead_look = '  /^ ^\ \n / * * \ \n V\ Y /V \n  / - \ \n /    | \nV__) ||'
-        pet.sounds.append('Haf Haf')
-        print("I'm a " + pet.name, '\n' + pet.look)
+class Dog(Pet):
+    def __init__(self):   
+        super().__init__()
+    pet = Pet()
+    pet.diet = ['beef', 'bone', 'carrot']
+    pet.name = 'big dog'
+    pet.look = '''
+     /^ ^\ 
+    / 0 0 \ 
+    V\ Y /V 
+     / - \ 
+    /    |
+    V__) ||'''
+    pet.dead_look = '''  
+     /^ ^\  
+    / * * \ 
+    V\ Y /V 
+     / - \ 
+    /    | 
+    V__) ||'''
+    pet.sounds.append('Haf Haf')
+    print(f"I'm a {pet.name}{pet.look}")
