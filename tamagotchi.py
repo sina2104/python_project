9a207b482410290a619acf40977bf3acf54b1e8d
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-05-17 15:11:23 +0300

    Update tamagotchi.py

--- tamagotchi.py
+++ tamagotchi.py
@@ -2,8 +2,6 @@ from animal import Cat, Dog
 
 from time import sleep
 
-from random import randrange
-
 
 class Play(Cat, Dog):
     """
@@ -30,30 +28,12 @@ class Play(Cat, Dog):
             break
         except input_errors:
             print("choose again:")
-    while True:
+    while not our_animal.pet.dead_mode(our_animal):
         our_animal.pet.clock_tick()
-        print(our_animal.pet, '\n' + our_animal.pet.sounds[first_element])
+        print(f"{our_animal.pet}\n{our_animal.pet.sounds[first_element]}")
         sleep(three_second)
-        if our_animal.pet.get_mood() == 'dead':
-            print('You had to care about me...')
-            print(our_animal.pet.dead_look)
-            break
-        if our_animal.pet.get_mood() == 'hungry':
-            food = input()
-            if food in our_animal.pet.diet:
-                our_animal.pet.feed()
-        elif our_animal.pet.get_mood() == 'bored':
-            entertain = input()
-            our_animal.pet.teach(entertain)
-            print(our_animal.pet.sounds[randrange(first_element + 1, len(our_animal.pet.sounds))])
-        elif our_animal.pet.get_mood() == 'hungry and board':
-            print('feed me then teach me something')
-            food = input()
-            if food in our_animal.pet.diet:
-                our_animal.pet.feed()
-            entertain = input()
-            our_animal.pet.teach(entertain)
-            print(our_animal.pet.sounds[randrange(first_element + 1, len(our_animal.pet.sounds))])
+        our_animal.pet.hungry_mode(our_animal)
+        our_animal.pet.bored_mode(our_animal)
 
 
 tamagotchi = Play()
