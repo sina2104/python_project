c500064a4bc6d08bd6c1d0d63d2fc025e88746c7
sina2104 <79207960+sina2104@users.noreply.github.com>
2021-05-18 20:27:37 +0300

    Update tamagotchi.py

--- tamagotchi.py
+++ tamagotchi.py
@@ -11,6 +11,11 @@ class Play(Cat, Dog):
         three_second - 3 секунды
         first_element - первый элемент
         input_errors - ошибки ввода
+        number - номер состояния
+        modes - состояния животное
+        mode_commands - комманды в соответствии с состоянием животного
+        feelings - состояния животного, о которых мы сообщаем хозяину
+        commands - комманды
     """
     three_second = 3
     first_element = 0
@@ -28,12 +33,29 @@ class Play(Cat, Dog):
             break
         except input_errors:
             print("choose again:")
-    while not our_animal.pet.dead_mode(our_animal):
+    commands = {
+        'our_animal.pet.happy_mode(our_animal)': our_animal.pet.happy_mode,
+        'our_animal.pet.hungry_mode(our_animal)': our_animal.pet.hungry_mode,
+        'our_animal.pet.bored_mode(our_animal)': our_animal.pet.bored_mode
+    }
+    while our_animal.pet.dead_mode(our_animal) != "daed":
+        modes_command = []
+        modes = []
+        number = 0
+        while number <= 3:
+            if our_animal.pet.get_mood(number) != "none":
+                modes.append(our_animal.pet.get_mood(number))
+            number += 1
+        feelings = modes[first_element]
         our_animal.pet.clock_tick()
-        print(f"{our_animal.pet}\n{our_animal.pet.sounds[first_element]}")
+        for i in range(len(modes)):
+            modes_command.append(f"our_animal.pet.{modes[i]}_mode(our_animal)")
+            if i != 0:
+                feelings += f" and {modes[i]}"
+        print(f"I feel {feelings}!")
         sleep(three_second)
-        our_animal.pet.hungry_mode(our_animal)
-        our_animal.pet.bored_mode(our_animal)
+        for el in modes_command:
+            commands[el](our_animal)
 
 
 tamagotchi = Play()
